fc3ca97d6d5ddf40b956b38bcc59d33430e70cd1
Guillermo Piñate <gpinate@gmail.com>
2024-12-16 22:54:48 +0100

    Upload

--- src/app/pages/pages/components/technologies/technologies.component.ts
+++ src/app/pages/pages/components/technologies/technologies.component.ts
@@ -9,11 +9,11 @@ import { PathTechnologies } from '../../interfaces/path-technologies.interface';
 export class TechnologiesComponent {
   public urls: PathTechnologies[] = [
     {
-      path: './assets/tecnologias/mongodb-ar21.svg',
+      path: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg',
       alt: 'Mongo Image',
     },
     {
-      path: './assets/tecnologias/expressjs-ar21.svg',
+      path: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg',
       alt: 'express Image',
     },
     {
@@ -21,7 +21,7 @@ export class TechnologiesComponent {
       alt: 'Angular Image',
     },
     {
-      path: './assets/tecnologias/nodejs.svg',
+      path: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg',
       alt: 'nodejs Image',
     },
     {
@@ -39,6 +39,98 @@ export class TechnologiesComponent {
     {
       path: './assets/tecnologias/logo-javascript.svg',
       alt: 'javascript Image',
+    },
+    {
+      path: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg',
+      alt: 'react',
+    },
+    {
+      path: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/amazonwebservices/amazonwebservices-original-wordmark.svg',
+      alt: 'aws',
+    },
+    {
+      path: 'https://www.vectorlogo.zone/logos/microsoft_azure/microsoft_azure-icon.svg',
+      alt: 'Azure',
+    },
+    {
+      path: 'https://www.vectorlogo.zone/logos/babeljs/babeljs-icon.svg',
+      alt: 'Babel',
+    },
+    {
+      path: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg',
+      alt: 'Bootstrap',
+    },
+    {
+      path: 'https://www.chartjs.org/media/logo-title.svg',
+      alt: 'Chartjs',
+    },
+    {
+      path: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg',
+      alt: 'Css',
+    },
+    {
+      path: 'https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg',
+      alt: 'tailwindcss',
+    },
+    {
+      path: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg',
+      alt: 'docker',
+    },
+    {
+      path: 'https://www.vectorlogo.zone/logos/figma/figma-icon.svg',
+      alt: 'figma',
+    },
+    {
+      path: 'https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg',
+      alt: 'firebase',
+    },
+    {
+      path: 'https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg',
+      alt: 'git',
+    },
+    {
+      path: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg',
+      alt: 'html5',
+    },
+    {
+      path: 'https://www.vectorlogo.zone/logos/jasmine/jasmine-icon.svg',
+      alt: 'jasmine',
+    },
+    {
+      path: 'https://raw.githubusercontent.com/detain/svg-logos/780f25886640cef088af994181646db2f6b1a3f8/svg/karma.svg',
+      alt: 'karma',
+    },
+    {
+      path: 'https://www.vectorlogo.zone/logos/jestjsio/jestjsio-icon.svg',
+      alt: 'jestjsio',
+    },
+    {
+      path: 'https://www.vectorlogo.zone/logos/jenkins/jenkins-icon.svg',
+      alt: 'jenkins',
+    },
+    {
+      path: 'https://www.vectorlogo.zone/logos/kubernetes/kubernetes-icon.svg',
+      alt: 'kubernetes',
+    },
+    {
+      path: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg',
+      alt: 'mysql',
+    },
+    {
+      path: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg',
+      alt: 'php',
+    },
+    {
+      path: 'https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg',
+      alt: 'getpostman',
+    },
+    {
+      path: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg',
+      alt: 'python',
+    },
+    {
+      path: 'https://cdn.worldvectorlogo.com/logos/jira-1.svg',
+      alt: 'jira',
     },
   ];
 }
